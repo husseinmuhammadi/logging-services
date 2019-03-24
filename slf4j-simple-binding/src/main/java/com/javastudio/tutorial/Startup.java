b30edd90278d98b1e9b8061b5e7cd4f8085857b4
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2019-03-24 13:14:18 +0430

    Slf4j simple binding

---
+++ slf4j-simple-binding/src/main/java/com/javastudio/tutorial/Startup.java
@@ -0,0 +1,13 @@
+package com.javastudio.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Startup {
+
+    private static Logger logger = LoggerFactory.getLogger(Startup.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world!");
+    }
+}
